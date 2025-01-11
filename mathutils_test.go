2a31318e228d93bc4dec14b7f7085b68d16043b5
Роман <romahomnh@gmail.com>
2025-01-12 00:39:35 +0700

    mathutils_test.go

--- mathutils_test.go
+++ mathutils_test.go
@@ -2,16 +2,46 @@ package mathutils
 
 import "testing"
 
+// Тестирование функции Add
 func TestAdd(t *testing.T) {
-    result := Add(3, 5)
-    if result != 8 {
-        t.Errorf("Expected 8, got %f", result)
+    tests := []struct {
+        a, b, expected int
+    }{
+        {1, 2, 3},
+        {-1, -1, -2},
+        {0, 0, 0},
+        {1000000, 1000000, 2000000},
+        {-1, 2, 1},
+    }
+
+    for _, test := range tests {
+        t.Run("", func(t *testing.T) {
+            result := Add(test.a, test.b)
+            if result != test.expected {
+                t.Errorf("Add(%d, %d) = %d; expected %d", test.a, test.b, result, test.expected)
+            }
+        })
     }
 }
 
+// Тестирование функции Sub
 func TestSub(t *testing.T) {
-    result := Sub(10, 4)
-    if result != 6 {
-        t.Errorf("Expected 6, got %f", result)
+    tests := []struct {
+        a, b, expected int
+    }{
+        {3, 2, 1},
+        {-1, -1, 0},
+        {0, 0, 0},
+        {1000000, 999999, 1},
+        {5, 7, -2},
+    }
+
+    for _, test := range tests {
+        t.Run("", func(t *testing.T) {
+            result := Sub(test.a, test.b)
+            if result != test.expected {
+                t.Errorf("Sub(%d, %d) = %d; expected %d", test.a, test.b, result, test.expected)
+            }
+        })
     }
 }
